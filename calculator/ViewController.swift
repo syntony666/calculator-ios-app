bf4c874db22a6fdc29e2b51c8093a1707bf32ebc
Tony <syntony666@outlook.com>
2021-04-26 15:06:44 +0800

    init layout

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -8,12 +8,68 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var caiculateResultLabel: UILabel!
+    
+    @IBOutlet weak var calculateProcessLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func zeroButton(_ sender: Any) {
+    }
+    
+    @IBAction func oneButton(_ sender: Any) {
+    }
+    
+    @IBAction func twoButton(_ sender: Any) {
+    }
+    
+    @IBAction func threeButton(_ sender: Any) {
+    }
+    
+    @IBAction func fourButton(_ sender: Any) {
+    }
+    
+    @IBAction func fiveButton(_ sender: Any) {
+    }
+    
+    @IBAction func sixButton(_ sender: Any) {
+    }
+    
+    @IBAction func sevenButton(_ sender: Any) {
+    }
+    
+    @IBAction func eightButton(_ sender: Any) {
+    }
+    
+    @IBAction func nineButton(_ sender: Any) {
+    }
+    
+    @IBAction func dotButton(_ sender: Any) {
+    }
+    
+    @IBAction func percentButton(_ sender: Any) {
+    }
+    
+    @IBAction func inverseNumberButton(_ sender: Any) {
+    }
+    
+    @IBAction func plusButton(_ sender: Any) {
+    }
+    
+    @IBAction func minusButton(_ sender: Any) {
+    }
+    
+    @IBAction func multipyButton(_ sender: Any) {
+    }
+    
+    @IBAction func divideButton(_ sender: Any) {
+    }
+    
+    @IBAction func allClearButton(_ sender: Any) {
+    }
+    
 }
-
